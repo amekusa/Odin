4c052a486a6f4624389545618c992ad5e3c44fd1
Satoshi Soma <noreply@amekusa.com>
2024-11-08 02:06:28 +0900

    add note

--- shell-mac.nix
+++ shell-mac.nix
@@ -11,3 +11,6 @@ pkgs.mkShell {
   ];
   shellHook="CXX=clang++";
 }
+
+# NOTE: default.nix:
+#       import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9706f4dd0e516de529fbd15bb8337e4a10d53307.tar.gz") # update: 2024-11-04
